4a5910c93b54a5c63c51d8cdc622b9dc0c0a0b1d
Jesse <jesserusso90@gmail.com>
2022-10-08 16:51:57 -0700

    Searches all pages and writes csv to desktop

--- scraper1/Listing.cs
+++ scraper1/Listing.cs
@@ -9,22 +9,35 @@ namespace scraper1
     internal class Listing
     {
         public string Description { get; } = "";
+        public string HouseType { get; set; } = "string>";
         public string Bedrooms { get; set; } = "0";
-        public string Link { get; } = "";
-        public string Price { get; } = "0";
         public string Baths { get; set; } = "0";
-        public Listing(string desc, string beds, string baths, string link, string price)
+        public string City { get; set; } = "";
+        public string Price { get; } = "0";
+        public string Link { get; set; } = "";
+        public Listing(string desc, string link, string price)
         {
             Description = desc;
-            Bedrooms = beds;
             Link = link;
             Price = price;
         }
 
-        public void setBedsBaths(string beds, string baths)
+        public void SetBedsBaths(string beds, string baths)
         {
             Bedrooms = beds;
             Baths = baths;
+        }
+        public void SetCity(string city)
+        {
+            City = city;
+        }
+        public void SetType(string type)
+        {
+            HouseType = type;
+        }
+        public string GetRecord()
+        {
+            return $"{Description}, {HouseType}, {Bedrooms}, {Baths}, {City}, {Price}, {Link},";
         }
     }
 }
--- scraper1/Program.cs
+++ scraper1/Program.cs
@@ -1,26 +1,51 @@
 ﻿using HtmlAgilityPack;
+using System.Globalization;
 using Fizzler.Systems.HtmlAgilityPack;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using CsvHelper;
 
 namespace scraper1
 {
     class Program
     {
-        private static string Pages { get; set; } = "";
+        private static int Pages { get; set; } = 0;
+        private static HtmlDocument MainHtml { get; set; } = new HtmlDocument();
+
+        private static string RentalPage { get; set;} = "https://classifieds.castanet.net/cat/rentals/";
+
+        private static int CurrentPage { get; set; } = 1;
         static async Task Main(string[] args)
         {
-            string rentalPage = "https://classifieds.castanet.net/cat/rentals/";
-            var html = await GetHtml(rentalPage);
-            List<Listing> adInfo = GetAds(html);
-            for(int i = 0; i < adInfo.Count; i++)
+            CultureInfo culture = new CultureInfo("en-US");
+            var html = await GetHtml(RentalPage);
+            MainHtml.LoadHtml(html);
+            Pages = CountPages(MainHtml);
+            List<Listing> adInfo = new();
+            
+            while(CurrentPage < 3)
             {
+                Console.Write($"\rGetting page: {CurrentPage}/{Pages}");
+                string pageModifier = $"?p={CurrentPage}";
+                adInfo.AddRange(GetAds(MainHtml));
+                html = await GetHtml(RentalPage + pageModifier);
+                MainHtml.LoadHtml(html);
+                CurrentPage++;
+            }
+            Console.WriteLine($"{Environment.NewLine}Getting individual rental details. This may take a while...");
+            for (int i = 0; i < adInfo.Count; i++)
+            {
+                Console.Write($"\rProgress: {i}/{adInfo.Count - 1}");
                 html = await GetHtml(adInfo[i].Link);
                 string[] details = ((string[])GetDetails(html));
-                adInfo[i].setBedsBaths(details[0], details[1]);
-                Console.WriteLine($"{adInfo[i].Description} {Environment.NewLine}{adInfo[i].Bedrooms} beds {Environment.NewLine}{adInfo[i].Baths} baths{Environment.NewLine}{adInfo[i].Price.Trim()}{Environment.NewLine}");
+                adInfo[i].SetBedsBaths(details[0], details[1]);
+                adInfo[i].SetCity(details[2]);
+                adInfo[i].SetType(details[3]);
+                adInfo[i].Link = "https://castanet.net" + adInfo[i].Link;
+                //Console.WriteLine($"{adInfo[i].Description} {Environment.NewLine}{adInfo[i].Bedrooms} beds {Environment.NewLine}{adInfo[i].Baths} baths{Environment.NewLine}{adInfo[i].Price.Trim()}{Environment.NewLine}");
             }
+            MakeCSV(adInfo);
         }
         private static Task<string> GetHtml(string link)
         {
@@ -29,19 +54,9 @@ namespace scraper1
             return client.GetStringAsync(link);
         }
 
-        private static List<Listing> GetAds(string html)
+        private static List<Listing> GetAds(HtmlDocument htmlDoc)
         {
             List<Listing> listingList = new List<Listing>();
-            var htmlDoc  = new HtmlDocument();
-            htmlDoc.LoadHtml(html);
-
-            //finds max pages of listings
-            var nav = htmlDoc.DocumentNode.SelectNodes("//div")
-                .Where(x => x.HasClass("navigation"))
-                .Select(x => x.Descendants("a").SkipLast(1));
-
-            Pages = nav.First().ElementAt(6).InnerText.ToString();
-
             //selects the individual ad nodes
             var ads = htmlDoc.DocumentNode
                 .SelectNodes("//a")
@@ -53,7 +68,7 @@ namespace scraper1
                 var name = ad.Descendants("h2").FirstOrDefault().InnerText;
                 var price = ad.Descendants("div").Where(x => x.GetClasses().Contains("price")).FirstOrDefault().InnerText;
                 var link = ad.Attributes["href"].Value.ToString();
-                listingList.Add(new Listing(name, "3","0",link, price));
+                listingList.Add(new Listing(name, link, price));
             }
             return listingList;
         }
@@ -61,19 +76,63 @@ namespace scraper1
         {
             var htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(html);
-            var details = htmlDoc.DocumentNode
+            var bedsBaths = htmlDoc.DocumentNode
                 .SelectNodes("//div")
                 .Where(x => x.HasClass("prod_right"))
                 .Select(x => x.Descendants("tr")).ToList();
 
-            string beds = details[0].ElementAt(0).Elements("td").ElementAt(1).InnerText.ToString().Trim();
-            string baths = details[0].ElementAt(1).Elements("td").ElementAt(1).InnerText.ToString().Trim();
-            string[] array = new[] { beds, baths };
+            string beds = bedsBaths[0].ElementAt(0).Elements("td").ElementAt(1).InnerText.ToString().Trim();
+            string baths = bedsBaths[0].ElementAt(1).Elements("td").ElementAt(1).InnerText.ToString().Trim();
+
+            var location = htmlDoc.DocumentNode
+                .SelectNodes("//div")
+                .Where(x => x.HasClass("prod_left"))
+                .Select(x => x.Descendants("tr")).ToList();
+            string city = location[0].ElementAt(2).Elements("td").ElementAt(1).InnerText.ToString().Trim().Split(",")[0];
+            string type = GetHouseType(htmlDoc);
+            string[] array = new[] { beds, baths, city, type };
             return array;
         }
-        private int CountPages(HtmlDocument html)
+        private static int CountPages(HtmlDocument html)
         {
-            return 0;
+            var nav = html.DocumentNode.SelectNodes("//div")
+                .Where(x => x.HasClass("navigation"))
+                .Select(x => x.Descendants("a").SkipLast(1));
+
+            int pageCount = int.Parse(nav.First().ElementAt(6).InnerText.ToString());
+            Console.WriteLine($"Found {pageCount} pages of ads");
+            return pageCount;
+        }
+
+        public static string GetHouseType(HtmlDocument html)
+        {
+            var crumbs = from c in html.DocumentNode.Descendants("a")
+                         where c.HasClass("ccrumbs")
+                         select c.InnerText;
+            string type;
+            if (crumbs.Any())
+            {
+                type = crumbs.First().ToString();
+                return type;
+            }
+            return "none";
+        }
+        public static void MakeCSV(List<Listing> list)
+        {
+            Console.WriteLine($"{Environment.NewLine}Writing csv file");
+            using (var writer = new StreamWriter($"C:\\Users\\jesse\\Desktop\\houses.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteHeader<Listing>();
+                csv.NextRecord();
+                int count = 1;
+                foreach(Listing item in list)
+                {
+                    csv.WriteRecord(item);
+                    csv.NextRecord();
+                    count++;
+                }
+            }
         }
 
     }
